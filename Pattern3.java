e345b5824284f92abda59afbceb7adb3367edbf3
Raj Kumar Ambadipelli <90890578+RajKumar-A91@users.noreply.github.com>
2021-09-17 13:04:37 +0530

    Pattern3 of is created
    This is the 2nd assignment of JavaTutorials.

---
+++ Pattern3.java
@@ -0,0 +1,15 @@
+public class Pattern3 
+{
+    public static void main(String []args)
+    {
+        for(char i=65;i<91;i++)
+        {
+            for(char j=65;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
