9525d639190fa47bb3154389b71d9615c393d3a9
Raj Kumar Ambadipelli <90890578+RajKumar-A91@users.noreply.github.com>
2021-11-14 15:13:49 +0530

    Static methods in Interfaces
    Understanding static methods in interfaces and 
    the way for accessing static methods in interfaces without any reference or object of interface

---
+++ StaticMethodinInterface.java
@@ -0,0 +1,16 @@
+interface Demo
+{
+    static void show()
+    {
+        System.out.println("hiii");
+    }
+}
+public class StaticMethodinInterface
+{
+    public static void main(String args[])
+    {
+        Demo.show();
+
+    }
+    
+}
